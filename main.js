ceedb4f9fc247a664802acc5ef61d32930bd4e9f
akrish4 <ananthakrishnan20121999@gmail.com>
2020-12-31 21:29:50 +0530

    cirtual piano js upload

---
+++ main.js
@@ -0,0 +1,21 @@
+document.addEventListener("keydown",
+    function(event) {
+        if (event.key == "a") {
+            console.log("The 'A' key is pressed.") }
+        else if (event.key == "s") {
+            console.log("The 'S' key is pressed.")}
+        else if(event.key == "d") {
+            console.log("The 'D' key is pressed.")}
+        else if (event.key == "f") {
+            console.log("The 'F' key is pressed.")}
+        else if (event.key == "g") {
+            console.log("The 'G' key is pressed.")}
+        else if (event.key == "h") {
+            console.log("The 'H' key is pressed.")}
+        else if (event.key == "j") {
+            console.log("The 'J' key is pressed.")}
+
+        else {
+            console.log("warning")
+        }
+    });
